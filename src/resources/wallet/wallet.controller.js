898a04ab4dc58351659e938e65bc81feb5ab1088
Alejo <avinetam@gmail.com>
2021-01-25 20:52:53 +0100

    more merge fixes

--- src/resources/wallet/wallet.controller.js
+++ src/resources/wallet/wallet.controller.js
@@ -3,16 +3,16 @@ const walletModel = require('./wallet.model');
 
 const getOne = async (req, res) => {
   try {
-    const user = await walletModel.getOne({id: req.params.id})
-    res.status(200).json(user)
+    const wallet = await walletModel.getOne({_id: req.params.id});
+    res.status(200).json(wallet);
   } catch (error) {
     if (error.message === "wallet not found") return res.status(404).json({error: "wallet not found"});
     res.status(500).json(error);
 }}
 
 const createOne = (req, res) => {
-
-  const newWallet = req.body;
+  console.log(req.body);
+  const newWallet = json(req.body);
   const walletUpdated = walletModel.create(newWallet)
   res.status(201).json(walletUpdated)
 };
--- src/resources/wallet/wallet.model.js
+++ src/resources/wallet/wallet.model.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 // Define model schema
 const walletModelSchema = mongoose.Schema({
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'WalletModel',
-      },
-  comment: String,
-  paymentMethod: Array,
+    author: String,
+    comment: String,
+    paymentMethod: Array,
+    saldo: Number
 });
 // Compile model from schema
 const Wallet = mongoose.model('WalletModel', walletModelSchema);
+
+
 const create = (wallet) => {
+    console.log(wallet);
   Wallet.create(wallet, function (err, docs) {
     if (err) {
       console.log(err);
@@ -19,9 +20,9 @@ const create = (wallet) => {
     }
   });
 };
-const get = async (id) => {
+const getOne = async (id) => {
   let query = { _id: id };
-  return await Newsletter.findOne(query);
+  return await Wallet.findOne(query);
 };
 const all = async () => {
   return await Wallet.find().populate('id');
@@ -36,7 +37,7 @@ const remove = (id) => {
     }
   });
 };
-const update = (id, updatewallet) => {
+const updateOne = (id, updateWallet) => {
   let query = { _id: id };
   Wallet.updateOne(query, updateWallet, function (err, docs) {
     if (err) {
@@ -48,8 +49,8 @@ const update = (id, updatewallet) => {
 };
 module.exports = {
   create,
-  update,
+  updateOne,
   remove,
-  get,
+  getOne,
   all,
 };
