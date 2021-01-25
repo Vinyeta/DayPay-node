c671520d5fd1da8b6cf31e3668ad77b61787de3c
Marcos Andaluz de Rosas <74211509+mandaluzr@users.noreply.github.com>
2021-01-25 21:07:59 +0100

    esperoquefuncione

--- src/resources/wallet/wallet.controller.js
+++ src/resources/wallet/wallet.controller.js
@@ -11,18 +11,15 @@ const getOne = async (req, res) => {
 }}
 
 const createOne = (req, res) => {
-  console.log(req.body);
-  const newWallet = json(req.body);
+  const newWallet = req.body;
   const walletUpdated = walletModel.create(newWallet)
   res.status(201).json(walletUpdated)
 };
-
 
 const updateOne = (req, res) => {
   const wallet = walletModel.updateOne(req.params.id, req.body);
   return res.status(200).json(wallet);
 };
-
 
 module.exports = {
   getOne,
--- src/server.js
+++ src/server.js
@@ -14,19 +14,16 @@ global.appRoot = path.resolve(__dirname);
 
 const app = express();
 
-app.disable('x-powered-by');
-app.use('/wallet', walletRouter);
-
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+app.disable('x-powered-by');
+app.use('/wallet', walletRouter);
+
 const start = async () => {
   try {
-
-  
-
     app.listen(config.port, () => {
       console.log(`REST API on http://localhost:${config.port}`);
     });
