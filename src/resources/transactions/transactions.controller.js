2c6ea5c0ed765f4306fc104054f9a0a1afa76d0b
Adrià Santos <adrsnts@gmail.com>
2021-03-19 19:58:15 +0100

    hehe

--- src/resources/transactions/transactions.controller.js
+++ src/resources/transactions/transactions.controller.js
@@ -1,8 +1,9 @@
 const transactionModel = require("./transactions.model");
 const walletModel = require("../wallet/wallet.model");
-const userModel = require('../users/users.model');
-const { validationResult } = require('express-validator');
+const userModel = require("../users/users.model");
+const { validationResult } = require("express-validator");
 const currency = require("../../Utils/moneyFormating");
+const jwt = require("jsonwebtoken");
 
 
 const getAll = async (req, res) => {
@@ -18,7 +19,6 @@ const getOne = async (req, res) => {
   return res.status(404).end();
 };
 
-
 const update = (req, res) => {
   const updateTransaction = req.body;
 
@@ -32,99 +32,140 @@ const update = (req, res) => {
 
 const remove = (req, res) => {
   const transactionWithoutTheDeleted = transactionModel.remove(req.params.id);
-
   return res.status(200).json(transactionWithoutTheDeleted);
 };
 
 const handleTransaction = async (payload) => {
-  payload = JSON.parse(payload);
+  const user = jwt.decode(req.headers.authorization.split(" ")[1]);
+  if (user._id == payload.sender) {payload = JSON.parse(payload);
   console.log(payload);
-    if (payload.amount < 0 ) {
-        return 'Amount not a postive number'
-      }
+  if (payload.amount < 0) {
+    return "Amount not a postive number";
+  }
   const sender = await walletModel.getOne(payload.sender);
-  const targetUser = await  userModel.getByEmail(payload.receiver);
-  const receiver = await walletModel.getByUser(targetUser._id)
+  const targetUser = await userModel.getByEmail(payload.receiver);
+  const receiver = await walletModel.getByUser(targetUser._id);
 
-  if (payload.sender == receiver._id) return 'Cannot send money to yourself'
+  if (payload.sender == receiver._id) return "Cannot send money to yourself";
 
   const newTransaction = {
-    "sender": payload.sender,
-    "receiver": receiver._id,
-    "amount": currency.EURO(payload.amount).format()
+    sender: payload.sender,
+    receiver: receiver._id,
+    amount: currency.EURO(payload.amount).format(),
   };
   const moneyToAddOrSubstract = currency.EURO(payload.amount); //validar primero si la wallet tiene el dinero que pretende enviar.
-  if (currency.EURO(sender.funds).value >= currency.EURO(moneyToAddOrSubstract).value) {
+  if (
+    currency.EURO(sender.funds).value >=
+    currency.EURO(moneyToAddOrSubstract).value
+  ) {
     const walletSuma = await walletModel.updateOne(receiver, {
       funds: currency.EURO(receiver.funds).add(moneyToAddOrSubstract).format(),
     });
     const walletResta = await walletModel.updateOne(sender, {
-      funds: currency.EURO(sender.funds).subtract(moneyToAddOrSubstract).format(),
+      funds: currency
+        .EURO(sender.funds)
+        .subtract(moneyToAddOrSubstract)
+        .format(),
     });
     const transactionCreated = transactionModel.create(newTransaction);
+  }} else {
+    return res.status(401);
   }
 };
 
 const getTransactionsBySender = async (req, res) => {
-  const outgoingTransactions = await transactionModel.getBySender(
-    req.params.id
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
   );
-  outgoingTransactions.map((e) =>{
-    const amountValue = currency.EURO(e.amount).value;
-    e.amount = currency.EURO(-amountValue).format();
-  }); 
-  outgoingTransactions.slice(0,10);
-  return res.status(200).json(outgoingTransactions);
+  if (verifyWallet._id == req.params.id) {
+    const outgoingTransactions = await transactionModel.getBySender(
+      req.params.id
+    );
+    outgoingTransactions.map((e) => {
+      const amountValue = currency.EURO(e.amount).value;
+      e.amount = currency.EURO(-amountValue).format();
+    });
+    outgoingTransactions.slice(0, 10);
+    return res.status(200).json(outgoingTransactions);
+  } else {
+    return res.status(401);
+  }
 };
 
 const getTransactionsByReceiver = async (req, res) => {
-  const incomingTransactions = await transactionModel.getByReceiver(
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
+  );
+  if (verifyWallet._id == req.params.id) {const incomingTransactions = await transactionModel.getByReceiver(
     req.params.id
   );
-  incomingTransactions.slice(0,10);
-  return res.status(200).json(incomingTransactions);
+  incomingTransactions.slice(0, 10);
+  return res.status(200).json(incomingTransactions);} else {
+    return res.status(401);
+  }
 };
 
 const getAllWalletTransactions = async (req, res) => {
-  const incomingTransactions = await transactionModel.getByReceiver(
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
+  );
+  if (verifyWallet._id == req.params.id) {const incomingTransactions = await transactionModel.getByReceiver(
     req.params.id
   );
   const outgoingTransactions = await transactionModel.getBySender(
     req.params.id
   );
-  outgoingTransactions.map((e) =>{
+  outgoingTransactions.map((e) => {
     const amountValue = currency.EURO(e.amount).value;
     e.amount = currency.EURO(-amountValue).format();
-  }); 
-
+  });
 
   let allTransactions = incomingTransactions.concat(outgoingTransactions);
   allTransactions.sort((a, b) => {
     var c = new Date(a.date);
     var d = new Date(b.date);
-    return d-c;
+    return d - c;
   });
 
-  allTransactions = allTransactions.slice(0,10);
- 
-  return res.status(200).json(allTransactions);
+  allTransactions = allTransactions.slice(0, 10);
+
+  return res.status(200).json(allTransactions);} else {
+    return res.status(401);
+
+  }
 };
 const getBySenderLastWeek = async (req, res) => {
-  try {
-    const outgoingTransactions = await transactionModel.getBySender$DateRange(req.params.id);
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
+  );
+  if (verifyWallet._id == req.params.id) { try {
+    const outgoingTransactions = await transactionModel.getBySender$DateRange(
+      req.params.id
+    );
     return res.status(200).json(outgoingTransactions);
   } catch (error) {
     console.log(error);
+  }} else {
+    return res.status(401);
+
   }
-}
+};
 const getByReceiverLastWeek = async (req, res) => {
-  try {
-    const ingoingTransactions = await transactionModel.getByReceiver$DateRange(req.params.id);
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
+  );
+  if (verifyWallet._id == req.params.id) {try {
+    const ingoingTransactions = await transactionModel.getByReceiver$DateRange(
+      req.params.id
+    );
     return res.status(200).json(ingoingTransactions);
   } catch (error) {
     console.log(error);
+  }} else {
+    return res.status(401);
+
   }
-}
+};
 
 module.exports = {
   update,
@@ -136,5 +177,5 @@ module.exports = {
   getTransactionsByReceiver,
   getAllWalletTransactions,
   getBySenderLastWeek,
-  getByReceiverLastWeek
+  getByReceiverLastWeek,
 };
--- src/resources/wallet/wallet.controller.js
+++ src/resources/wallet/wallet.controller.js
@@ -1,83 +1,135 @@
-const walletModel = require('./wallet.model');
-const TransactionsModel = require('../transactions/transactions.model');
+const jwt = require("jsonwebtoken");
+const walletModel = require("./wallet.model");
+const TransactionsModel = require("../transactions/transactions.model");
 const currency = require("../../Utils/moneyFormating");
 
-
-
 const getOne = async (req, res) => {
-  try {
-    const wallet = await walletModel.getOne({ _id: req.params.id });
-    res.status(200).json(wallet);
-  } catch (error) {
-    if (error.message === "wallet not found") return res.status(404).json({ error: "wallet not found" });
-    res.status(500).json(error);
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
+  );
+
+  if (verifyWallet._id == req.params.id) {
+    try {
+      const wallet = await walletModel.getOne({ _id: req.params.id });
+      res.status(200).json(wallet);
+    } catch (error) {
+      if (error.message === "wallet not found")
+        return res.status(404).json({ error: "wallet not found" });
+      res.status(500).json(error);
+    }
+  } else {
+    return res.status(401);
   }
-}
+};
 
 const createOne = (req, res) => {
   const newWallet = req.body;
   newWallet.funds = currency.EURO(newWallet.funds).format();
-  const walletUpdated = walletModel.create(newWallet)
-  res.status(201).json(walletUpdated)
+  const walletUpdated = walletModel.create(newWallet);
+  res.status(201).json(walletUpdated);
 };
 
-const update = (req, res) => {
-  const updatedBody = {
-    "comment" : req.body.comment,
-    "paymentMethod": req.body.paymentMethod,
-    "funds": currency.EURO(req.body.funds).format()
+const update = async (req, res) => {
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
+  );
+
+  if (verifyWallet._id == req.params.id) {
+    const updatedBody = {
+      comment: req.body.comment,
+      paymentMethod: req.body.paymentMethod,
+      funds: currency.EURO(req.body.funds).format(),
+    };
+    const wallet = walletModel.updateOne(req.params.id, updatedBody);
+    return res.status(200).json(wallet);
+  } else {
+    return res.status(401);
   }
-  const wallet = walletModel.updateOne(req.params.id, updatedBody);
-  return res.status(200).json(wallet);
 };
 
 const getByUserId = async (req, res) => {
   try {
-    const wallet = await walletModel.getByUser(req.params.id);
-    res.status(200).json(wallet);
+    const user = jwt.decode(req.headers.authorization.split(" ")[1]);
+    if (user._id === req.params.id) {
+      const wallet = await walletModel.getByUser(req.params.id);
+      return res.status(200).json(wallet);
+    } else {
+      return res.status(401);
+    }
   } catch (error) {
-    if (error.message === "wallet not found") return res.status(404).json({ error: "wallet not found" });
+    if (error.message === "wallet not found")
+      return res.status(404).json({ error: "wallet not found" });
     res.status(500).json(error);
   }
-}
+};
 
 const getBalance = async (req, res) => {
   try {
-    const wallet = await walletModel.getOne({ _id: req.params.id });
-    res.status(200).json(wallet.funds);
+    const verifyWallet = await walletModel.getByUser(
+      jwt.decode(req.headers.authorization.split(" ")[1])
+    );
+    if (verifyWallet._id == req.params.id) {
+      const wallet = await walletModel.getOne({ _id: req.params.id });
+      console.log("verified");
+      res.status(200).json(wallet.funds);
+    } else {
+      return res.status(401);
+    }
   } catch (error) {
-    if (error.message === "wallet not found") return res.status(404).json({ error: "wallet not found" });
+    if (error.message === "wallet not found")
+      return res.status(404).json({ error: "wallet not found" });
     res.status(500).json(error);
   }
-}
+};
 
 const weeklyIncrement = async (req, res) => {
-  const currentFunds = await walletModel.getOne({ _id: req.params.id });
-  const outcomeTransactions = await TransactionsModel.getBySender$DateRange(req.params.id)
-  const incomeTransactions = await TransactionsModel.getByReceiver$DateRange(req.params.id)
-  let outcomeSum = currency.EURO(0);
-  outcomeTransactions.forEach(element => {
-    outcomeSum = currency.EURO(element.amount).add(outcomeSum);
-  });
-  let incomeSum = currency.EURO(0);
-  incomeTransactions.forEach(element => {
-    incomeSum = currency.EURO(element.amount).add(incomeSum);
-  });
-  let sum = currency.EURO(incomeSum).subtract(outcomeSum);
-  const lastWeekFunds = currency.EURO(currentFunds.funds).subtract(sum);
-  let increment = currency.EURO(sum).value * 100 / currency.EURO(lastWeekFunds).value
-  return res.status(200).json(increment);
-}
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
+  );
 
-const stripePayment = async (req,res) => {
-  const wallet = await walletModel.getOne(req.params.id);
-  const updatedBody = {
-    "funds": currency.EURO(wallet.funds).add(req.body.amount).format()
+  if (verifyWallet._id == req.params.id) {
+    const currentFunds = await walletModel.getOne({ _id: req.params.id });
+    const outcomeTransactions = await TransactionsModel.getBySender$DateRange(
+      req.params.id
+    );
+    const incomeTransactions = await TransactionsModel.getByReceiver$DateRange(
+      req.params.id
+    );
+    let outcomeSum = currency.EURO(0);
+    outcomeTransactions.forEach((element) => {
+      outcomeSum = currency.EURO(element.amount).add(outcomeSum);
+    });
+    let incomeSum = currency.EURO(0);
+    incomeTransactions.forEach((element) => {
+      incomeSum = currency.EURO(element.amount).add(incomeSum);
+    });
+    let sum = currency.EURO(incomeSum).subtract(outcomeSum);
+    const lastWeekFunds = currency.EURO(currentFunds.funds).subtract(sum);
+    let increment =
+      (currency.EURO(sum).value * 100) / currency.EURO(lastWeekFunds).value;
+    return res.status(200).json(increment);
+  } else {
+    return res.status(401);
   }
-  console.log(updatedBody.funds); 
-  const updatedWallet = walletModel.updateOne(req.params.id, updatedBody);
-  return res.status(200).json(updatedWallet);
-}
+};
+
+const stripePayment = async (req, res) => {
+  const verifyWallet = await walletModel.getByUser(
+    jwt.decode(req.headers.authorization.split(" ")[1])
+  );
+
+  if (verifyWallet._id == req.params.id) {
+    const wallet = await walletModel.getOne(req.params.id);
+    const updatedBody = {
+      funds: currency.EURO(wallet.funds).add(req.body.amount).format(),
+    };
+    console.log(updatedBody.funds);
+    const updatedWallet = walletModel.updateOne(req.params.id, updatedBody);
+    return res.status(200).json(updatedWallet);
+  } else {
+    return res.status(401);
+  }
+};
 
 module.exports = {
   getOne,
@@ -86,5 +138,5 @@ module.exports = {
   getByUserId,
   getBalance,
   weeklyIncrement,
-  stripePayment
+  stripePayment,
 };
